5ea70eef42bd6e34c7a17d86b1d6bcf3be91bdbe
Matthieu Corrand <matthieu.corrand@maestroqa.com>
2022-06-15 15:36:51 -0400

    checking authentication based on header - next need to map client id to user id

--- bin/server.js
+++ bin/server.js
@@ -5,6 +5,8 @@
  */
 const WebSocket = require('ws')
 const http = require('http')
+const crypto = require('crypto')
+const { MongoClient } = require('mongodb')
 const wss = new WebSocket.Server({ noServer: true })
 const setupWSConnection = require('./utils.js').setupWSConnection
 
@@ -16,18 +18,46 @@ const server = http.createServer((request, response) => {
   response.end('okay')
 })
 
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/maestroqa'
+const mongo = new MongoClient(mongoURL)
+
 wss.on('connection', setupWSConnection)
 
-server.on('upgrade', (request, socket, head) => {
-  // You may check auth of request here..
-  // See https://github.com/websockets/ws#client-authentication
-  /**
-   * @param {any} ws
-   */
-  const handleAuth = ws => {
-    wss.emit('connection', ws, request)
+function hashToken (loginToken) {
+  const hashedToken = crypto.createHash('sha256').update(loginToken).digest('base64')
+  console.log(loginToken, hashedToken)
+  return hashedToken
+}
+
+async function authenticate (request, next) {
+  const rawHeaders = request.rawHeaders // an array instead of obj k , v are in successive indexes
+  const loginHeaderIndex = request.rawHeaders.indexOf('loginToken')
+  if (loginHeaderIndex === -1) {
+    next('missing loginToken header')
   }
-  wss.handleUpgrade(request, socket, head, handleAuth)
+  const loginToken = rawHeaders[loginHeaderIndex + 1]
+  const hashedToken = hashToken(loginToken)
+  await mongo.connect()
+  const user = await mongo.db('maestroqa').collection('users').findOne({ 'services.resume.loginTokens.hashedToken': hashedToken })
+  if (!user) {
+    next('user not found')
+  }
+  next(false, user)
+}
+
+// const map = new Map()
+server.on('upgrade', async function upgrade (request, socket, head) {
+  await authenticate(request, function next (err, user) {
+    if (err) {
+      socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n')
+      socket.destroy()
+      return
+    }
+    // map.set(user._id, wss)
+    wss.handleUpgrade(request, socket, head, function done (ws) {
+      wss.emit('connection', ws, request)
+    })
+  })
 })
 
 server.listen(port, () => {
